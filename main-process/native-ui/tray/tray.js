16a2f0ff4485b0a6559e45463547c027091e48d1
jayden <jxdjayden@gmail.com>
2019-06-13 20:37:26 +0800

    update

--- main-process/native-ui/tray/tray.js
+++ main-process/native-ui/tray/tray.js
@@ -6,6 +6,12 @@ let appIcon = null
 ipcMain.on('put-in-tray', (event) => {
   const iconName = process.platform === 'win32' ? 'windows-icon.png' : 'iconTemplate.png'
   const iconPath = path.join(__dirname, iconName)
+  const balloonContent = {
+    title: '标题测试',
+    content: 'content',
+    icon: iconPath
+  }
+
   appIcon = new Tray(iconPath)
 
   const contextMenu = Menu.buildFromTemplate([{
@@ -17,6 +23,7 @@ ipcMain.on('put-in-tray', (event) => {
 
   appIcon.setToolTip('Electron Demo in the tray.')
   appIcon.setContextMenu(contextMenu)
+  appIcon.displayBalloon(balloonContent)
 })
 
 ipcMain.on('remove-tray', () => {
